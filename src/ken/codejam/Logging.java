c0f03fbd522d7019a26a00844413c20349653f8e
khanhnq2 <khanhnq2@vng.com.vn>
2016-04-13 20:41:06 +0700

    logging

--- src/ken/codejam/Logging.java
+++ src/ken/codejam/Logging.java
@@ -2,6 +2,9 @@ package ken.codejam;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 import ken.codejam.utils.AutoParseInputProblem;
 import ken.codejam.utils.AutoParseTestCase;
@@ -39,14 +42,14 @@ public class Logging extends AutoParseInputProblem{
 			
 			StringBuilder builder = new StringBuilder();
 			for (int i = 0; i < N; i ++){
-				builder.append("\n").append(small(i));
+				builder.append("\n").append(large(i));
 			}
 			try {
 				output.write("Case #" + order + ":" + builder.toString() + "\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			System.out.println(System.currentTimeMillis() - a);
+			System.out.println("TIME: " + (System.currentTimeMillis() - a));
 		}
 		
 		int[][] cache;
@@ -84,6 +87,70 @@ public class Logging extends AutoParseInputProblem{
 		
 		long checkSide(long x1, long y1, long x2, long y2, long x, long y){
 			return ((x1 - x2) * (y - y1) - (y1 - y2) * (x - x1));
+		}
+		
+		
+		
+		int large(int i){
+			if (N == 1) return 0;
+			ArrayList<Double> lT = new ArrayList<>();
+			for (int j = 0; j < N; j ++){
+				if (i != j){
+					long dX = T[i][0] - T[j][0];
+					long dY = T[i][1] - T[j][1];
+//					double a = (double) dX/ (Math.sqrt(dX * dX + dY * dY));
+					lT.add(Math.atan2(dY, dX));
+				}
+			}
+			
+			Collections.sort(lT);
+			
+			int min = 3001;
+			for (int j = 0; j < lT.size(); j ++){
+				double op = lT.get(j) + Math.PI;
+				if (op > Math.PI){
+					op -= Math.PI * 2;
+				}
+				int pos1 = binSearchMin(lT, 0, lT.size() -1, op);
+				int pos2 = binSearchMax(lT, 0, lT.size() -1, op);
+				int side1 = 0, side2 = 0;
+				if (j <= pos1){
+					side1 = pos1 - j;
+				} else {
+					side1 = N-1 - j + pos1;
+				}
+				
+				if (pos1 <= pos2){
+					side2 = N-1 - side1 - (pos2 - pos1);
+				} else {
+					side2 = N-1 -side1 - (N-1 - pos1 + pos2);
+				}
+				
+				int s = side1 > side2 ? side2 : side1;
+				if (s == 0) return 0;
+				if (min > s) min = s;
+			}
+			
+			return min;
+		}
+		
+		int binSearchMin(ArrayList<Double> LT, int low, int heigh, double val){
+			if (low +1 >= heigh) return low;
+			int mid = (low + heigh)/2;
+			if (LT.get(mid) < val){
+				return binSearchMin(LT, mid, heigh, val);
+			} else {
+				return binSearchMin(LT, low, mid, val);
+			}
+		}
+		int binSearchMax(ArrayList<Double> LT, int low, int heigh, double val){
+			if (low +1 >= heigh) return low;
+			int mid = (low + heigh)/2;
+			if (LT.get(mid) <= val){
+				return binSearchMax(LT, mid, heigh, val);
+			} else {
+				return binSearchMax(LT, low, mid, val);
+			}
 		}
 
 		@Override
